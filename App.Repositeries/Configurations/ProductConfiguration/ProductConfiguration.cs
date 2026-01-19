3646adcefd11954b6959b80232f3d3b47abb80b4
Furkan Arslan <2200656003nku.edu.tr>
2026-01-19 16:46:13 +0300

    Add project files.

---
+++ App.Repositeries/Configurations/ProductConfiguration/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using App.Repositeries.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace App.Repositeries.Configurations.ProductConfiguration
+{
+    public class ProductConfiguration :IEntityTypeConfiguration<Product>
+
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+            builder.Property(p => p.Price).IsRequired().HasColumnType("decimal(18,2)");
+            builder.Property(p => p.Stock).IsRequired().HasMaxLength(50);
+        }
+
+
+    }
+}
---
+++ App.Repositeries/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using App.Repositeries.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace App.Repositeries.Data
+{
+    public class AppDbContext (DbContextOptions<AppDbContext> option) : DbContext(option)
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+           modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+        }
+
+
+
+    }
+}
